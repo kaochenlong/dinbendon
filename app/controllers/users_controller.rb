985fbd4ded584d1f4c09a7e03b86fe59d5b7918d
Eddie Kao <eddie@digik.com.tw>
2020-03-27 16:47:38 +0800

    add user registration part 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,15 @@ class UsersController < ApplicationController
   def login
   end
 
+  def sign_up
+  end
+
+  def registration
+    user = params[:user]
+    if user[:password] == user[:password_confirm]
+      redirect_to "/login"
+    else
+      redirect_to "/sign_up"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "/login", to: "users#login"
+  get "/sign_up", to: "users#sign_up"
+  post "/sign_up", to: "users#registration"
 
   root "welcome#index"
 end
