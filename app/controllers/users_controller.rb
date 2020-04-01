1c99644f7a2cdb6cdcfeb0ab40536a501315e68c
Eddie Kao <eddie@digik.com.tw>
2020-04-01 16:56:21 +0800

    add logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,39 @@
 class UsersController < ApplicationController
   def login
+    @user = User.new
   end
 
   def sign_up
     @user = User.new
   end
 
+  def sign_in
+    user = User.find_by(email: user_params[:email], 
+                        password: user_params[:password])
+
+    if user 
+      session[:ccc9527] = user.email
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
+  end
+
   def registration
     @user = User.new(user_params)
     if @user.save
-      # 成功
+      session[:ccc9527] = @user.email
       # TODO: 密碼加密
-      # TODO: 幫登入
-      redirect_to "/"
+      redirect_to root_path
     else
       # 失敗
       render :sign_up
     end
+  end
+
+  def logout
+    session[:ccc9527] = nil
+    redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "/login", to: "users#login"
+  post "/login", to: "users#sign_in"
+  delete "/logout", to: "users#logout"
+
   get "/sign_up", to: "users#sign_up"
   post "/sign_up", to: "users#registration"
 
