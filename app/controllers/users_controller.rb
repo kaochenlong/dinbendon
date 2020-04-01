664b5239c1a2bb3e5882a0a4eb2aa2a528771196
Eddie Kao <eddie@digik.com.tw>
2020-04-01 12:43:46 +0800

    add user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,32 @@
 class UsersController < ApplicationController
-
   def login
   end
 
   def sign_up
+    @user = User.new
   end
 
   def registration
-    user = params[:user]
-    if user[:password] == user[:password_confirm]
-      redirect_to "/login"
+    if user_params[:password] == user_params[:password_confirm]
+      @user = User.new(user_params)
+      if @user.save
+        # 成功
+        # TODO: 密碼加密
+        # TODO: 幫登入
+        redirect_to "/"
+      else
+        # 失敗
+        render :sign_up
+      end
     else
       redirect_to "/sign_up"
     end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, 
+                                 :password, 
+                                 :password_confirm)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :password_confirm
+
+  validates :email, presence: true
+  validates :password, presence: true
 end
