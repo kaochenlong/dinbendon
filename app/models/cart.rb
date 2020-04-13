74a29924e2e0c92941a8aaa25ec921ac29a703f5
Eddie Kao <eddie@digik.com.tw>
2020-04-13 16:50:15 +0800

    WIP - 基本功能 2

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,9 +1,18 @@
 class Cart
+  def initialize
+    @items = []
+  end
+
   def add_item(item_id)
+    @items << item_id
   end
 
   def empty?
-    false
+    @items.empty?
+  end
+
+  def items
+    @items
   end
 end
 
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -7,6 +7,16 @@ RSpec.describe Cart, type: :model do
       cart.add_item(1)
       expect(cart.empty?).to be false
     end
+
+    it "加了相同種類的商品到購物車裡，購買項目不會增加，數量會改變" do
+      cart = Cart.new
+
+      3.times { cart.add_item(1) }
+      2.times { cart.add_item(2) }
+      2.times { cart.add_item(1) }
+
+      expect(cart.items.count).to be 2
+    end
   end
 
   describe "進階功能" do
