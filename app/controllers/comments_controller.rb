530e0bcf77c2e2ade6ab070b04cbbc85e8e65031
Eddie Kao <eddie@digik.com.tw>
2020-04-20 16:10:19 +0800

    add comment with ajax way

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,12 @@ class CommentsController < ApplicationController
     @comment = @item.comments.build(comment_params.merge(user: current_user))
     
     if @comment.save
-      redirect_to item_path(params[:item_id]), notice: 'ok'
+      # /app/views/comments/create.html.erb
+      # /app/views/comments/create.js.erb
+
+      # render js: "alert('hi')"
+      # render json: {name: '1111', age: 18}
+      # redirect_to item_path(params[:item_id]), notice: 'ok'
     else
       render 'items/show'
     end
