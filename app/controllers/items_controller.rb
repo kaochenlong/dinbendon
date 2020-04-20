5831f2dee63eccfa667929a6ce77f2030c39f7ad
Eddie Kao <eddie@digik.com.tw>
2020-04-20 14:29:14 +0800

    show comments

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,7 @@ class ItemsController < ApplicationController
 
   def show
     @comment = Comment.new
+    @comments = @item.comments
   end
 
   def new
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :item
 
   validates :content, presence: true, length: { minimum: 2 }
-end
 
+  default_scope { where(deleted_at: nil).order(id: :desc) }
+end
