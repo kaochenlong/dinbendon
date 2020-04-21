3897ab0352edcdbd2ccf002b78dcd91dce2dce12
Eddie Kao <eddie@digik.com.tw>
2020-04-21 15:43:38 +0800

    issue #15

--- app/frontend/controllers/item_controller.js
+++ app/frontend/controllers/item_controller.js
@@ -1,41 +1,29 @@
 import { Controller } from "stimulus"
-import ax from "axios"
+import Rails from "@rails/ujs"
 
 export default class extends Controller {
   static targets = ["icon"]
 
-  initialize() {
-    this.clicked = false;
-  }
-
   heart(e) {
     e.preventDefault();
 
-    const csrfToken = document.querySelector('[name=csrf-token]').content
-    ax.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
-
     let item_id = document.querySelector('#item_id').value;
 
-    ax.post(`/api/v1/items/${item_id}/favorite`)
-      .then(resp => {
-        if (resp.data.status === "favorited") {
+    Rails.ajax({
+      url: `/api/v1/items/${item_id}/favorite`, 
+      type: 'POST', 
+      success: resp => {
+        if (resp.status === "favorited") {
           this.iconTarget.classList.remove('far');
           this.iconTarget.classList.add('fas');
         } else {
           this.iconTarget.classList.remove('fas');
           this.iconTarget.classList.add('far');
         }
-      })
-      .catch( err => {
+      }, 
+      error: err => {
         console.log(err);
-      })
-    //
-    // api request
-
-    // if (this.clicked) {
-    //   this.clicked = false
-    // } else {
-    //   this.clicked = true
-    // }
+      } 
+    })
   }
 }
