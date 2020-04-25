14d844844b03321ca70133e2be9513f8de0fe1d0
Linda Wu <pinky510100@gmail.com>
2020-04-25 15:16:14 +0800

    issue #11

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -6,6 +6,11 @@ require("@rails/activestorage").start()
 require("styles")
 require("scripts")
 
+require("jquery")
+require("bootstrap")
+
+window.jQuery = $
+window.$ = $
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,3 +3,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+const webpack = require('webpack')
+ environment.plugins.prepend('Provide',
+   new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery'
+   })
+)
