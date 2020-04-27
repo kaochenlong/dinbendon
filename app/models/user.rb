2877caa67f1aaf377b90d5e804994e7ff5a39629
yuchan <lumir1031@gmail.com>
2020-04-28 02:12:02 +0800

    增加更新使用者資料頁面

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, 
+  validates :password, presence: true,
                        confirmation: true, 
                        length: { minimum: 4 }
 
