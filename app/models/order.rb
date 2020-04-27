6ec9fcc641f407145b74087ac066470ef3c68df6
Eddie Kao <eddie@digik.com.tw>
2020-04-27 14:25:17 +0800

    WIP - add callback

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,23 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
+
+  before_create :create_order_num
+
+  private
+
+  def create_order_num
+    self.order_num = num_generator
+  end
+
+  def num_generator
+    # 實作內容
+    year = Date.today.year
+    month = Date.today.month
+    day = Date.today.day
+    code = [*0..9, *'A'..'Z'].sample(6).join
+
+    # 20200427A8E1E9
+    "#{year}#{"%02d" % month}#{"%02d" % day}#{code}"
+  end
 end
