8fedaa50122a665550979101e202d6f29c47cd09
Eddie Kao <eddie@digik.com.tw>
2020-04-27 16:48:08 +0800

    WIP - add transaction

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,8 +12,27 @@ class OrdersController < ApplicationController
 
     if @order.save
       # 刷卡
-      # 清空購物車
-      redirect_to root_path, notice: 'ok'
+      gateway = Braintree::Gateway.new(
+        environment: :sandbox,
+        merchant_id: 'jwb8xz5rcmwgrxx8',
+        public_key: 'd22xqv3rnmm5m5kx',
+        private_key: '0d86c945075de33c3e1eafbf8cd2506e'
+      )
+
+      result = gateway.transaction.sale(
+        amount: current_cart.total,
+        payment_method_nonce: params[:order][:nonce]
+      )
+
+      if result.success?
+        # 成功
+        # 清空購物車
+        redirect_to root_path, notice: '交易成功'
+      else
+        # 失敗
+        redirect_to root_path, alert: '交易失敗'
+      end
+
     else
       redirect_to root_path, notice: 'not ok'
     end
